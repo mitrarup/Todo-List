001a442869b5b614b65e4a0ca90a809eec2ef078
Arup-mitra <arupmitra141@gmail.com>
2026-06-08 00:44:05 +0530

    feat: handle project and todo user interaction

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import "./style.css";
-
+import { renderProjects,renderTodos } from "./modules/renderDisplay.js";
+import "./modules/events.js"
 
  if (process.env.NODE_ENV !== 'production') {
    console.log('Looks like we are in development mode!');
  }
- 
+
+
+renderProjects();
+renderTodos();
---
+++ src/modules/events.js
@@ -0,0 +1,90 @@
+import { state } from "./state.js";
+import { projectManager } from "./project.js";
+import { todoManager } from "./todo.js";
+import { renderProjects,renderTodos } from "./renderDisplay.js";
+
+
+const sidebar = document.querySelector(".sidebar");
+const projectBtn = document.querySelector(".project_btn")
+const projectInputContainer = document.querySelector(".projectInputContainer");
+const projectInput = document.querySelector(".projectInput");
+const container = document.querySelector(".container");
+
+sidebar.addEventListener("click", (e)=>{
+    // if any project
+    if( e.target.classList.contains("project")){
+        state.selectedProjectId = e.target.id ;
+        renderTodos();
+    }
+})
+// if add project button is clicked
+projectBtn.addEventListener("click", (e)=>{
+    
+    projectInputContainer.classList.remove("hidden");
+    projectInput.focus();
+})
+projectInputContainer.addEventListener("keydown", (e) => {
+
+  if (e.key === "Enter" &&
+      projectInput.value.trim() !== "") {
+
+    state.selectedProjectId = projectManager.createProject( projectInput.value );
+
+    renderProjects();
+    renderTodos();
+    projectInput.value = "";
+
+    projectInputContainer.classList.add("hidden");
+  }
+});
+
+
+
+//Taking dialog and form elements
+const addTodoBtn = document.querySelector(".todo_btn");
+const cancel = document.getElementById("cancel");
+const dialog = document.getElementById("my_dialog");
+const myForm = document.getElementById("myform");
+
+
+addTodoBtn.addEventListener("click", () => {
+    dialog.showModal();
+})
+cancel.addEventListener("click", () => {
+    dialog.close();
+})
+
+myForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    //mapping the form input values
+    const title = myForm.querySelector('[name="title"]').value;
+    const description = myForm.querySelector('[name="description"]').value;
+    const priority = myForm.querySelector('[name="priority"]').value;
+    const duedate = myForm.querySelector('[name="duedate"]').value;
+
+
+    todoManager.createTodo( title,description,duedate,priority);
+    dialog.close();
+    myForm.reset();
+    renderTodos();
+});
+
+
+container.addEventListener("click", (e)=>{
+    // if( e.target.classList.contains("details_btn")){
+        
+    // }
+    // if( e.target.classList.contains("edit_btn")){
+
+
+    // }
+    // delete todo
+    if( e.target.classList.contains("delete_btn")){
+        // get the todo
+        let parent = e.target.closest(".todo");
+        // delete the todo
+        todoManager.deleteTodo( parent.id );
+        // update UI
+        renderTodos();
+    }
+})
