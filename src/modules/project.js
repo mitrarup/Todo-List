50a2bdb24fe3343b63a6b671c22ca5e6e9e5a810
Arup-mitra <arupmitra141@gmail.com>
2026-06-08 00:07:04 +0530

    Feat: Add todo & project creation and deletion logic

---
+++ src/modules/project.js
@@ -0,0 +1,27 @@
+import { state } from "./state.js";
+
+class Project{
+  constructor(name){
+    this.id = crypto.randomUUID();
+    this.name = name;
+    this.todos = [];
+  }
+}
+export const projectManager = ( ()=> {
+  function createProject(name){
+    const project = new Project(name);
+    state.projects.push(project);
+
+    return project.id;
+  }
+  function deleteProject(uid){
+    const index = state.projects.findIndex(project => project.id == uid);
+    if(index !== -1){
+        state.projects.splice(index,1);
+    }
+  }
+  return { createProject,deleteProject };
+})()
+
+// Default project
+state.selectedProjectId = projectManager.createProject("Default");
---
+++ src/modules/todo.js
@@ -0,0 +1,37 @@
+import { state } from "./state.js";
+import "./project.js"
+
+class Todo {
+  constructor(title,description,duedate,priority){
+      this.id = crypto.randomUUID();
+      this.title = title;
+      this.description = description;
+      this.duedate = duedate;
+      this.priority = priority;
+      this.checklist = false;
+    }
+    toggleStatus() {
+        this.checklist = !this.checklist;
+    }
+ }
+export const todoManager = (  ()=> {
+    //get the current project through state.selectedProjectId from state.js
+    const current_project = state.projects.find(project => project.id == state.selectedProjectId);
+
+    function createTodo(title,description,duedate,priority){
+        const newtodo = new Todo(title,description,duedate,priority);
+        current_project.todos.push(newtodo);
+    }
+
+    function deleteTodo(uid){
+
+      current_project.todos =  current_project.todos.filter(todo => todo.id != uid);
+    }
+
+    return { createTodo,deleteTodo };
+})()
+// creating some todos
+todoManager.createTodo("Stay Consistent","Have to stay consisted","22 MAY 2026","high");
+todoManager.createTodo("DSA Lecture & Practice","Have to remain consistent","1st JUN 2026","medium");
+todoManager.createTodo("Attend College Lectures","Need to increase the Attendence","17 JULY 2026","low");
+
